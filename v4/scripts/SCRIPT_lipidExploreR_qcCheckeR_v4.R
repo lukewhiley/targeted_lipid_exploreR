0820bc833bcb51db9d1879bc797900fca410607a
Luke Whiley <luke.whiley@murdoch.edu.au>
2024-09-06 14:03:52 +0800

    reorganised folders

--- v4/scripts/SCRIPT_lipidExploreR_qcCheckeR_v4.R
+++ v4/scripts/SCRIPT_lipidExploreR_qcCheckeR_v4.R
@@ -1572,7 +1572,7 @@ openxlsx::write.xlsx(
 ## 5.2. html report -------------------------
 
 ### 5.2.a. download template ----
-fileConn<-file(paste0(master_list$project_details$project_dir, "/html_report/lipid_exploreR_report_templatev4.R"))
+fileConn<-file(paste0(master_list$project_details$project_dir, "/html_report/lipid_exploreR_report_template_v4.R"))
 writeLines(httr::GET(url = paste0(master_list$project_details$github_master_dir, "/templates/TEMPLATE_lipidExploreR_report_v3.3.R")) %>%
              httr::content(as = "text"), fileConn)
 close(fileConn)
